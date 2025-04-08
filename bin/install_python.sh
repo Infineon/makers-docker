cbf809dc455aeee9b534f385b9c72960405bf00f
Jens Bargfrede <Jens.Bargfrede@infineon.com>
2025-04-08 18:23:47 +0200

    Updates of workflow and docker input files.

--- bin/install_python.sh
+++ bin/install_python.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-apt-get install -y black python3-pip python3 python3-yaml python3-requests python3-schema
+apt-get install -y black python3 python3-pip python3-requests python3-schema python3-serial python3-yaml
