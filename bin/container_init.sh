1066c44e89f20b7792c7afa2cb7b1c73e78f8e64
bargfred <jens.bargfrede@infineon.com>
2025-04-24 13:00:24 +0200

    Test push (#10)
    * Using branch test_push to generate image for testing and use JSON notation for dockerfile CMD command.
    
    * Fixes to workflow and docker file.
    
    * Removed unused docker workflow.
    
    * Fixes to workflow and docker file.
    
    * Added new python and linux packages and updated arduino_install script to use latest version of Infineon xmc.
    
    * docker/makers-dockerfile
    
    * Updates of workflow and docker input files.
    
    * Updates of workflow and docker input files.
    
    * Updates of workflow and docker input files.
    
    * Updates of workflow and docker input files.
    
    * Updates of workflow and docker input files.
    
    * Updates of workflow and docker input files.
    
    * Updates of workflow and docker input files.
    
    * Updates of workflow and docker input files.
    
    * Updates of workflow and docker input files.
    
    * Adding more tools as well as Unity.
    
    * Updates of workflow and docker input files.
    
    * Minor update of docker setup.

--- bin/container_init.sh
+++ bin/container_init.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
 
+apt-get update
+
 # Install additional packages
-apt-get install -y apt-utils cmake curl git gnupg make lsb-release software-properties-common
+apt-get install -y apt-utils cmake curl expect-dev git gnupg make lsb-release software-properties-common udev
+
+# expect-dev to install unbuffer
+# lsof to install lsof
+# psmisc to install fuser
+# udev to install udevadm
+# usbutils to install lsusb 
 
 # apt-get install -y clang-format clang-tidy clang-tools clang cppcheck gcc-arm-none-eabi binutils-arm-none-eabi
 # libffi-dev pkg-config
@@ -24,3 +32,12 @@ apt purge cppcheck
 
 cd /opt
 mkdir bin
+
+
+# Create dummy repo in order to be able to create link.
+# On the Lab-PC the full repo is available and will be used as the mounted directory from the Lab-PC has precedence.
+mkdir -p /opt/makers-hil/src/python/hil
+
+touch /opt/makers-hil/src/python/hil/runQuery.py
+chmod uog+rx /opt/makers-hil/src/python/hil/runQuery.py
+ln -s /opt/makers-hil/src/python/hil/runQuery.py /opt/bin/
--- bin/install_arduino_cli.sh
+++ bin/install_arduino_cli.sh
@@ -5,10 +5,22 @@ cd /opt
 
 curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | BINDIR=/opt/bin sh -s ${ARDUINO_CLI_VERSION}
 
-arduino-cli config init --config-file /root/.arduino15/arduino-cli.yaml
-arduino-cli config set --config-file /root/.arduino15/arduino-cli.yaml board_manager.additional_urls https://github.com/Infineon/XMC-for-Arduino/releases/download/V${XMC4ARDUINO_VERSION}/package_infineon_index.json
+#ln -s /opt/bin/arduino-cli /opt/bin/arduino-cli.exe
+ln -s `which arduino-cli` /opt/bin/arduino-cli.exe
+
+arduino-cli config init --config-file /root/.arduino15/arduino-cli.yaml --overwrite
+
+### latest
+arduino-cli config set --config-file /root/.arduino15/arduino-cli.yaml board_manager.additional_urls https://github.com/Infineon/XMC-for-Arduino/releases/latest/download/package_infineon_index.json
+### Vx.y.z
+#arduino-cli config set --config-file /root/.arduino15/arduino-cli.yaml board_manager.additional_urls https://github.com/Infineon/XMC-for-Arduino/releases/download/V${XMC4ARDUINO_VERSION}/package_infineon_index.json
+
 arduino-cli core update-index --config-file /root/.arduino15/arduino-cli.yaml
-arduino-cli core install --config-file /root/.arduino15/arduino-cli.yaml Infineon:xmc@${XMC4ARDUINO_VERSION}
+
+### latest version
+arduino-cli core install --config-file /root/.arduino15/arduino-cli.yaml Infineon:xmc
+### a version already downloaded
+#arduino-cli core install --config-file /root/.arduino15/arduino-cli.yaml Infineon:xmc@${XMC4ARDUINO_VERSION}
 
 arduino-cli board listall --config-file /root/.arduino15/arduino-cli.yaml Infineon:xmc
 
@@ -23,3 +35,58 @@ arduino-cli board listall --config-file /root/.arduino15/arduino-cli.yaml Infine
 
 # arduino-cli board listall Infineon
 # arduino-cli board listall
+
+
+
+# arduino-cli config init --config-file /home/jensb/.arduino15/arduino-cli.yaml --overwrite
+
+# arduino-cli config set --config-file /home/jensb/.arduino15/arduino-cli.yaml board_manager.additional_urls https://github.com/Infineon/XMC-for-Arduino/releases/latest/download/package_infineon_index.json
+# #arduino-cli config set --config-file /home/jensb/.arduino15/arduino-cli.yaml board_manager.additional_urls https://github.com/Infineon/XMC-for-Arduino/releases/download/V3.4.1/package_infineon_index.json
+
+# arduino-cli core update-index --config-file /home/jensb/.arduino15/arduino-cli.yaml
+
+# #arduino-cli core install --config-file /home/jensb/.arduino15/arduino-cli.yaml Infineon:xmc@3.5.1
+# #@${XMC4ARDUINO_VERSION}
+# arduino-cli core install --config-file /home/jensb/.arduino15/arduino-cli.yaml Infineon:xmc
+
+# arduino-cli board listall --config-file /home/jensb/.arduino15/arduino-cli.yaml Infineon:xmc
+
+
+
+
+# ### Install JLink for XMC
+# SEGGER_VERSION=V824
+
+# mkdir /opt/SEGGER
+# cd /opt/SEGGER
+
+# # wget "https://www.segger.com/downloads/jlink/JLink_Linux_${SEGGER_VERSION}_x86_64.tgz"
+
+# # wget --post-data="agreementValue=Agree" "https://www.segger.com/downloads/jlink/JLink_Linux_${SEGGER_VERSION}_x86_64.tgz" --save-cookies cookie.txt --keep-session-cookies --delete-after
+# # wget --load-cookies=cookie.txt 'https://www.segger.com/downloads/jlink/JLink_Linux_${SEGGER_VERSION}_x86_64.tgz/accept_license_agreement=accepted'
+
+# cp /myLocalWorkingDir/JLink_Linux_${SEGGER_VERSION}_x86_64.tgz .
+
+
+# tar zxvf JLink_Linux_${SEGGER_VERSION}_x86_64.tgz
+# chmod a-w JLink_Linux_V824_x86_64
+
+# ln -s JLink_Linux_${SEGGER_VERSION}_x86_64/JLink*Exe /opt/bin
+
+# mkdir -p /etc/udev/rules.d/
+# cp JLink_Linux_${SEGGER_VERSION}_x86_64/99-jlink.rules /etc/udev/rules.d/
+
+# /lib/systemd/systemd-udevd --daemon
+
+# udevadm control -R
+# udevadm trigger --action=remove --attr-match=idVendor=1366 --subsystem-match=usb
+# udevadm trigger --action=add    --attr-match=idVendor=1366 --subsystem-match=usb
+
+# cd /myLocalWorkingDir
+
+
+
+cd /opt
+git clone https://github.com/ThrowTheSwitch/Unity.git
+
+cd /myLocalWorkingDir
