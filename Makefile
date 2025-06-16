55df370696158a8ea310a23099e16365cd6987de
Jens Bargfrede <Jens.Bargfrede@infineon.com>
2025-06-16 17:08:23 +0200

    Updates of workflow and docker input files.

---
+++ Makefile
@@ -0,0 +1,62 @@
+
+CONTAINER_TAG=v0.0.3
+TEST_CONTAINER_TAG=test
+
+WORKFLOW_TEST_TAG=v0.0.3
+
+
+# store github password safely in local password file with owner only access !
+build-docker-login:
+	cat ~/.secrets/DOCKERHUB_PASSWORD | docker login -u ifxmakers --password-stdin
+
+
+# building the docker image from file Dockerfile using the github project sources of install scripts in the main branch
+build-local-docker-container: build-docker-login
+	docker build -t makers-docker:$(CONTAINER_TAG) -t makers-docker:latest -f Dockerfile docker 2>&1 | tee docker_gen.log
+	docker image tag makers-docker:$(CONTAINER_TAG) ifxmakers/makers-docker:$(CONTAINER_TAG) 2>&1 | tee -a docker_gen.log
+	docker image tag makers-docker:latest ifxmakers/makers-docker:latest 2>&1 | tee -a docker_gen.log
+	docker image push ifxmakers/makers-docker:$(CONTAINER_TAG) 2>&1 | tee -a docker_gen.log
+
+
+# building the docker image from file Dockerfile.test using the local sources of install scripts
+build-test-docker-container: build-docker-login
+	docker build -t makers-docker:$(TEST_CONTAINER_TAG) -f Dockerfile.test . 2>&1 | tee -a docker_test_gen.log
+	docker image tag makers-docker:$(TEST_CONTAINER_TAG) ifxmakers/makers-docker:$(TEST_CONTAINER_TAG) 2>&1 | tee -a docker_test_gen.log
+	docker image push ifxmakers/makers-docker:$(TEST_CONTAINER_TAG) 2>&1 | tee -a docker_test_gen.log
+
+
+# build the image with tag "push" from github source branch "test_push" - if there are changes in the Dockerfile or the bin or .github/workflows directory
+test-workflow: build-docker-login
+	git push --delete origin $(WORKFLOW_TEST_TAG)
+	git tag -d $(WORKFLOW_TEST_TAG)
+	git add Dockerfile .github/workflows/* bin/*
+	git commit -m "Updates of workflow and docker input files."
+	git tag -a -m "Release $(WORKFLOW_TEST_TAG) tag." $(WORKFLOW_TEST_TAG)
+	git push --follow-tags
+
+
+##############################################################################################################################################################
+#TAG=push
+#TAG=latest
+TAG=test
+
+#DOCKER_REGISTRY=dockerregistry-v2.vih.infineon.com/ifxmakers/makers-docker:$(TAG)
+DOCKER_REGISTRY=ifxmakers/makers-docker:$(TAG)
+GHCR_REGISTRY=ghcr.io/infineon/makers-docker:$(TAG)
+
+REGISTRY=$(DOCKER_REGISTRY)
+# REGISTRY=$(GHCR_REGISTRY)
+
+
+### Setting DOCKER variable to empty string => containers not used
+### Setting DOCKER variable to "docker run ..." => containers used
+DOCKER=
+DOCKER=docker run --rm -it --privileged $(DEVICES) -v $(PWD):/myLocalWorkingDir:rw -v /opt/makers-hil:/opt/makers-hil:ro -v /opt/hil:/opt/hil:ro -w /myLocalWorkingDir $(REGISTRY)
+
+
+pull-container:
+	docker pull $(REGISTRY)
+
+
+run-container-bash: pull-container
+	$(DOCKER) bash
--- bin/install_arduino_cli.sh
+++ bin/install_arduino_cli.sh
@@ -8,85 +8,46 @@ curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.
 #ln -s /opt/bin/arduino-cli /opt/bin/arduino-cli.exe
 ln -s `which arduino-cli` /opt/bin/arduino-cli.exe
 
-arduino-cli config init --config-file /root/.arduino15/arduino-cli.yaml --overwrite
 
-### latest
-arduino-cli config set --config-file /root/.arduino15/arduino-cli.yaml board_manager.additional_urls https://github.com/Infineon/XMC-for-Arduino/releases/latest/download/package_infineon_index.json
-### Vx.y.z
-#arduino-cli config set --config-file /root/.arduino15/arduino-cli.yaml board_manager.additional_urls https://github.com/Infineon/XMC-for-Arduino/releases/download/V${XMC4ARDUINO_VERSION}/package_infineon_index.json
+arduino-cli config init --config-file ${ARDUINO_CONFIG_FILE}  --overwrite
+#arduino-cli config init --config-file /root/.arduino15/arduino-cli.yaml  --overwrite
 
-arduino-cli core update-index --config-file /root/.arduino15/arduino-cli.yaml
-
-### latest version
-arduino-cli core install --config-file /root/.arduino15/arduino-cli.yaml Infineon:xmc
-### a version already downloaded
-#arduino-cli core install --config-file /root/.arduino15/arduino-cli.yaml Infineon:xmc@${XMC4ARDUINO_VERSION}
-
-arduino-cli board listall --config-file /root/.arduino15/arduino-cli.yaml Infineon:xmc
+arduino-cli core update-index --config-file ${ARDUINO_CONFIG_FILE}
 
 
-# arduino-cli config set board_manager.additional_urls https://github.com/Infineon/arduino-core-psoc/releases/download/V${PSOC4ARDUINO_VERSION}/package_psoc_index.json
-# arduino-cli board listall Infineon:psoc
+# XMC
+### install latest version
+#arduino-cli core install --config-file ${ARDUINO_CONFIG_FILE} Infineon:xmc --additional-urls https://github.com/Infineon/XMC-for-Arduino/releases/latest/download/package_infineon_index.json
+### install vx.y.z
+#arduino-cli config set --config-file ${ARDUINO_CONFIG_FILE} board_manager.additional_urls https://github.com/Infineon/XMC-for-Arduino/releases/download/V${XMC4ARDUINO_VERSION}/package_infineon_index.json
+
+# PSoC
+### install latest version
+# arduino-cli config set --config-file ${ARDUINO_CONFIG_FILE} board_manager.additional_urls https://github.com/Infineon/arduino-core-psoc6/releases/latest/download/package_psoc6_index.json
+### install vx.y.z
+#arduino-cli config set --config-file ${ARDUINO_CONFIG_FILE} board_manager.additional_urls https://github.com/Infineon/XMC-for-Arduino/releases/download/V${PSOC4ARDUINO_VERSION}/package_infineon_index.json
+
+
+### install latest versions
+arduino-cli core install --config-file ${ARDUINO_CONFIG_FILE} Infineon:xmc --additional-urls https://github.com/Infineon/XMC-for-Arduino/releases/latest/download/package_infineon_index.json
+arduino-cli core install --config-file ${ARDUINO_CONFIG_FILE} infineon:psoc6 --additional-urls https://github.com/Infineon/arduino-core-psoc6/releases/latest/download/package_psoc6_index.json
+
+### install a version for a core already installed
+#arduino-cli core install --config-file ${ARDUINO_CONFIG_FILE} Infineon:xmc@${XMC4ARDUINO_VERSION}
+
+
+# list cores
+arduino-cli board listall --config-file ${ARDUINO_CONFIG_FILE} Infineon
+
 
 # arduino-cli core search arduino
 # arduino-cli core install arduino:avr@1.8.6
 # arduino-cli core install arduino:renesas_uno@1.2.2
 # ~/.arduino15/packages/arduino/hardware/renesas_uno/1.2.2/post_install.sh
 
-# arduino-cli board listall Infineon
-# arduino-cli board listall
 
-
-
-# arduino-cli config init --config-file /home/jensb/.arduino15/arduino-cli.yaml --overwrite
-
-# arduino-cli config set --config-file /home/jensb/.arduino15/arduino-cli.yaml board_manager.additional_urls https://github.com/Infineon/XMC-for-Arduino/releases/latest/download/package_infineon_index.json
-# #arduino-cli config set --config-file /home/jensb/.arduino15/arduino-cli.yaml board_manager.additional_urls https://github.com/Infineon/XMC-for-Arduino/releases/download/V3.4.1/package_infineon_index.json
-
-# arduino-cli core update-index --config-file /home/jensb/.arduino15/arduino-cli.yaml
-
-# #arduino-cli core install --config-file /home/jensb/.arduino15/arduino-cli.yaml Infineon:xmc@3.5.1
-# #@${XMC4ARDUINO_VERSION}
-# arduino-cli core install --config-file /home/jensb/.arduino15/arduino-cli.yaml Infineon:xmc
-
-# arduino-cli board listall --config-file /home/jensb/.arduino15/arduino-cli.yaml Infineon:xmc
-
-
-
-
-# ### Install JLink for XMC
-# SEGGER_VERSION=V824
-
-# mkdir /opt/SEGGER
-# cd /opt/SEGGER
-
-# # wget "https://www.segger.com/downloads/jlink/JLink_Linux_${SEGGER_VERSION}_x86_64.tgz"
-
-# # wget --post-data="agreementValue=Agree" "https://www.segger.com/downloads/jlink/JLink_Linux_${SEGGER_VERSION}_x86_64.tgz" --save-cookies cookie.txt --keep-session-cookies --delete-after
-# # wget --load-cookies=cookie.txt 'https://www.segger.com/downloads/jlink/JLink_Linux_${SEGGER_VERSION}_x86_64.tgz/accept_license_agreement=accepted'
-
-# cp /myLocalWorkingDir/JLink_Linux_${SEGGER_VERSION}_x86_64.tgz .
-
-
-# tar zxvf JLink_Linux_${SEGGER_VERSION}_x86_64.tgz
-# chmod a-w JLink_Linux_V824_x86_64
-
-# ln -s JLink_Linux_${SEGGER_VERSION}_x86_64/JLink*Exe /opt/bin
-
-# mkdir -p /etc/udev/rules.d/
-# cp JLink_Linux_${SEGGER_VERSION}_x86_64/99-jlink.rules /etc/udev/rules.d/
-
-# /lib/systemd/systemd-udevd --daemon
-
-# udevadm control -R
-# udevadm trigger --action=remove --attr-match=idVendor=1366 --subsystem-match=usb
-# udevadm trigger --action=add    --attr-match=idVendor=1366 --subsystem-match=usb
-
-# cd /myLocalWorkingDir
-
-
-
+# install Unity
 cd /opt
 git clone https://github.com/ThrowTheSwitch/Unity.git
 
-cd /myLocalWorkingDir
+
